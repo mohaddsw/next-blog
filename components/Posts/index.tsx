bdc07f27100a9b6b0af70493a5e47923597901a1
mohaddese.r <mohaddese.r@behinrahkar.com>
2023-08-11 13:50:23 +0330

    get articles data

--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -2,17 +2,14 @@ import {Card, Flex, Loader, Pagination, Tabs} from '@mantine/core';
 import {  IconMessageCircle } from '@tabler/icons-react';
 import {useQuery} from "react-query";
 import {getAllArticles} from "@/services/Articles";
-import {Article} from '@/services/Articles'
+import type {Article} from '@/types/articles'
 import PostCard from "@/components/Posts/PostCard";
-import {useState} from "react";
 
 
 const Posts=()=>{
-    const [activePage, setPage] = useState(1);
     const { data, isLoading,  } = useQuery('Articles', getAllArticles);
-    const length=data?.articlesCount/10
     return(
-         <Card>
+         <Card className="mb-5">
              <Tabs color="pink" defaultValue="AllPosts">
                  <Tabs.List>
                      <Tabs.Tab value="AllPosts" icon={<IconMessageCircle size="0.8rem" />}>All Posts</Tabs.Tab>
@@ -21,7 +18,7 @@ const Posts=()=>{
                  <Tabs.Panel value="AllPosts" pt="xs">
                      {
                          isLoading? <Flex className="justify-center p-2"> <Loader color="pink" /></Flex>:
-                             data?.articles.map((article: Article)=>{
+                             data?.map((article: Article)=>{
                                  return <PostCard key={article.slug} article={article}/>
                              })
 
@@ -29,9 +26,6 @@ const Posts=()=>{
                  </Tabs.Panel>
 
              </Tabs>
-             <Flex className="justify-center">
-                 <Pagination className="mb-5" total={length} color="pink" size="xs" radius="xs" withEdges value={activePage} onChange={setPage} />
-             </Flex>
          </Card>
         );
 
--- services/Articles.ts
+++ services/Articles.ts
@@ -1,32 +1,9 @@
 import axios from '../plugins/axios'
 import {AxiosResponse} from "axios";
+import type {ApiResponse} from "@/types/articles";
 
-export interface Article {
-    slug: string;
-    title: string;
-    description: string;
-    body: string;
-    tagList: string[];
-    createdAt: string;
-    updatedAt: string;
-    favorited: boolean;
-    favoritesCount: number;
-    author: {
-        username: string;
-        bio: string | null;
-        image: string;
-        following: boolean;
-    };
-}
 
-interface ApiResponse {
-    data: {
-        articles: Article[];
-        articlesCount:number;
-
-    };
-}
 export const getAllArticles=async(): Promise<AxiosResponse<ApiResponse>> =>{
     const response =await axios.get<ApiResponse>('/articles')
-    return response.data;
+    return response.data.articles;
 }

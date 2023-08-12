4e3b1f9e31c4437cc16fbc5aea71ac26b04b764f
mohaddese.r <mohaddese.r@behinrahkar.com>
2023-08-12 20:07:38 +0330

    slug

--- pages/Posts/[slug].tsx
+++ pages/Posts/[slug].tsx
@@ -1,4 +1,7 @@
+import {useRouter} from "next/router";
 const SinglePost=()=>{
+    const router=useRouter()
+    console.log(router)
     return <div>single post</div>
 }
 export default SinglePost
